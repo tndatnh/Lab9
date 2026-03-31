9a95d2b3494662c2216785d4eee9a400b1ebf1ac
tndatnh <110185333+tndatnh@users.noreply.github.com>
2026-03-31 15:00:35 +0700

    Refactor CartTest to use ConfigReader for credentials
    Updated CartTest to use credentials from ConfigReader instead of hardcoded values.

--- src/test/java/tests/CartTest.java
+++ src/test/java/tests/CartTest.java
@@ -1,5 +1,6 @@
 package tests;
 import framework.base.BaseTest;
+import framework.config.ConfigReader;
 import framework.pages.CartPage;
 import framework.pages.InventoryPage;
 import framework.pages.LoginPage;
@@ -7,11 +8,16 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CartTest extends BaseTest {
+    
     @Test(description = "Thêm 1 sản phẩm vào giỏ - kiểm tra badge số lượng")
     public void testAddItemToCart() {
+        // ✅ Đọc credential từ ConfigReader
+        String username = ConfigReader.getInstance().getUsername();
+        String password = ConfigReader.getInstance().getPassword();
+        
         // Fluent Interface: chuỗi method liên tiếp
         InventoryPage inventoryPage = new LoginPage(getDriver())
-                .login("standard_user", "secret_sauce");
+                .login(username, password);
 
         inventoryPage.addFirstItemToCart();
 
@@ -21,8 +27,11 @@ public class CartTest extends BaseTest {
 
     @Test(description = "Kiểm tra sản phẩm xuất hiện trong CartPage")
     public void testItemAppearsInCart() {
+        String username = ConfigReader.getInstance().getUsername();
+        String password = ConfigReader.getInstance().getPassword();
+        
         CartPage cartPage = new LoginPage(getDriver())
-                .login("standard_user", "secret_sauce")
+                .login(username, password)
                 .addFirstItemToCart()
                 .goToCart(); // Fluent Interface hoạt động!
 
@@ -32,8 +41,11 @@ public class CartTest extends BaseTest {
 
     @Test(description = "Xóa sản phẩm khỏi giỏ - giỏ trở thành rỗng")
     public void testRemoveItemFromCart() {
+        String username = ConfigReader.getInstance().getUsername();
+        String password = ConfigReader.getInstance().getPassword();
+        
         CartPage cartPage = new LoginPage(getDriver())
-                .login("standard_user", "secret_sauce")
+                .login(username, password)
                 .addFirstItemToCart()
                 .goToCart()
                 .removeFirstItem();
